893e27ab78d6f05a17ebfb57b32e4a85a8c3a442
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-07 10:41:32 -0400

    set state isnt working

---
+++ src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+export default function TodoForm({handleTodo}) {
+    const [todo, setTodo] = useState("")
+    return (
+        <div>
+            <form onSubmit={e => {
+                e.preventDefault()
+                handleTodo(todo)
+                }}>
+                <label for="todo">
+                    What needs to be done?
+                    <input type="text" id="todo" name="todo" onChange={e => {setTodo(e.target.value)}}></input>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
